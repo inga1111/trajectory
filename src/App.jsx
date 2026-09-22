ca0889c56a10d3e226e7e8eb2b4024d817e00c6c
inga1111 <2582423@students.wits.ac.za>
2026-09-22 15:49:50 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
+import Questionnaire from "./pages/Questionnaire/Questionnaire";
+
 
 function App() {
   return (
@@ -9,8 +11,8 @@ function App() {
       <Routes>
 
         <Route path="/" element={<Landing />} />
-
         <Route path="/login" element={<Login />} />
+        <Route path="/questionnaire" element={<Questionnaire />} />
 
       </Routes>
     </BrowserRouter>
